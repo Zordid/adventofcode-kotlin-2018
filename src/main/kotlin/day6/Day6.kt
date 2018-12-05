f120c5f6fbab122f8384fa85028f13d5d26f3edc
Zordid <Zordid@users.noreply.github.com>
2018-12-05 16:45:30 +0100

    Prepare day 6

---
+++ src/main/kotlin/day6/Day6.kt
@@ -0,0 +1,18 @@
+package day6
+
+import shared.readPuzzle
+
+fun part1(puzzle: List<String>): Any {
+    return ""
+}
+
+fun part2(puzzle: List<String>): Any {
+    return ""
+}
+
+fun main(args: Array<String>) {
+    val puzzle = readPuzzle(6)
+
+    println(part1(puzzle))
+    println(part2(puzzle))
+}
