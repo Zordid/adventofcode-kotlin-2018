9b950550c44520f4033c2da6be0ea0543df4d281
Zordid <Zordid@users.noreply.github.com>
2018-12-05 13:24:49 +0100

    Improve code for day 5

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,51 +1,40 @@
 package day5
 
 import shared.readPuzzle
-import java.util.*
 
-infix fun Char.matches(other: Char) =
-    (isLowerCase() && other.isUpperCase() || isUpperCase() && other.isLowerCase()) &&
-            toLowerCase() == other.toLowerCase()
+/* Brilliant idea copied from Peter Tseng! */
+private const val caseDifference = 'a'.toInt() - 'A'.toInt()
 
-fun String.removeAllUnitsOf(type: Char): List<Char> = toCharArray().filter { it.toLowerCase() != type }
+infix fun Char.matches(other: Char) = Math.abs(this.toInt() - other.toInt()) == caseDifference
 
-fun String.polymerReaction(): Int = toCharArray().toList().polymerReaction()
+fun String.removeAllUnitsOf(type: Char) = asSequence().filter { it.toLowerCase() != type }
 
-fun List<Char>.polymerReaction(): Int {
-    val list = LinkedList(this)
-    var remaining = size
-
-    val iterator = list.listIterator()
-    var previousUnit = if (iterator.hasNext()) iterator.next() else return 0
-    while (iterator.hasNext()) {
-        val currentUnit = iterator.next()
-        if (previousUnit matches currentUnit) {
-            iterator.remove()
-            iterator.previous()
-            iterator.remove()
-            remaining -= 2
-            if (remaining > 0)
-                previousUnit = if (iterator.hasPrevious()) iterator.previous() else iterator.next()
-        } else {
-            previousUnit = currentUnit
-        }
+fun Sequence<Char>.polymerReaction(): String {
+    val reacted = StringBuilder()
+    for (c in this) {
+        if (reacted.isNotEmpty() && reacted.last() matches c)
+            reacted.setLength(reacted.length - 1)
+        else
+            reacted.append(c)
     }
-    return remaining
+    return reacted.toString()
 }
 
+fun String.polymerReaction()= asSequence().polymerReaction()
+
 fun part1(polymer: String): Any {
-    return polymer.polymerReaction()
+    return polymer.polymerReaction().length
 }
 
 fun part2(polymer: String): Any {
-    return polymer.toCharArray().filter { it.isLowerCase() }.toSet()
-        .map { it to polymer.removeAllUnitsOf(it).polymerReaction() }
+    return polymer.asSequence().filter { it.isLowerCase() }.distinct()
+        .map { it to polymer.removeAllUnitsOf(it).polymerReaction().length }
         .minBy { it.second }!!
 }
 
 fun main(args: Array<String>) {
-    val puzzle = readPuzzle(5)
+    val polymer = readPuzzle(5).single()
 
-    println(part1(puzzle.single()))
-    println(part2(puzzle.single()))
+    println(part1(polymer))
+    println(part2(polymer))
 }
--- src/test/kotlin/day5/Day5KtTest.kt
+++ src/test/kotlin/day5/Day5KtTest.kt
@@ -8,19 +8,27 @@ internal class Day5KtTest {
     private val polymer = "dabAcCaCBAcCcaDA"
 
     @Test
+    fun preparation() {
+        assertTrue('b' matches 'B')
+        assertFalse('a' matches 'B')
+        assertEquals("Hao Wet!", "Hallo Welt!".removeAllUnitsOf('l').joinToString(""))
+    }
+
+    @Test
     fun part1() {
-        assertEquals(0, "aA".polymerReaction())
-        assertEquals(0, "aAbB".polymerReaction())
-        assertEquals(0, "baAB".polymerReaction())
-        assertEquals(10, polymer.polymerReaction())
+        assertEquals("", "aA".polymerReaction())
+        assertEquals("", "aAbB".polymerReaction())
+        assertEquals("", "baAB".polymerReaction())
+        assertEquals("Hao Welt!", "HalLLlo WeabBcdEeDCAlt!".polymerReaction())
+        assertEquals(10, day5.part1(polymer))
     }
 
     @Test
     fun part2() {
-        assertEquals(6, polymer.removeAllUnitsOf('a').polymerReaction())
-        assertEquals(8, polymer.removeAllUnitsOf('b').polymerReaction())
-        assertEquals(4, polymer.removeAllUnitsOf('c').polymerReaction())
-        assertEquals(6, polymer.removeAllUnitsOf('d').polymerReaction())
+        assertEquals(6, polymer.removeAllUnitsOf('a').polymerReaction().length)
+        assertEquals(8, polymer.removeAllUnitsOf('b').polymerReaction().length)
+        assertEquals(4, polymer.removeAllUnitsOf('c').polymerReaction().length)
+        assertEquals(6, polymer.removeAllUnitsOf('d').polymerReaction().length)
         assertEquals('c' to 4, day5.part2(polymer))
     }
 
