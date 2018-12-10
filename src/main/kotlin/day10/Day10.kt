d21212bac86b80c1ca55be2ed28c759818aacd2b
Zordid <Zordid@users.noreply.github.com>
2018-12-10 06:35:29 +0100

    Clean up day 10

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -4,49 +4,38 @@ import shared.extractAllInts
 import shared.minMax
 import shared.readPuzzle
 
-data class Point(val p: Pair<Int, Int>, val v: Pair<Int, Int>) {
-    constructor(n: List<Int>) : this(n[0] to n[1], n[2] to n[3])
-}
+operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) =
+    first + other.first to second + other.second
 
-operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
-    return first + other.first to second + other.second
-}
+fun Pair<Int, Int>.toRange() = first..second
+fun Pair<Int, Int>.size() = Math.abs(second - first)
 
-fun part1(puzzle: List<String>): Any {
-    val data = puzzle.map { Point(it.extractAllInts().toList()) }
+fun part1and2(puzzle: List<List<Int>>): Any {
+    val (initialPositions, velocities) =
+            puzzle.map { (it[0] to it[1]) to (it[2] to it[3]) }.unzip()
 
-    var p = data.map { it.p }
-    val v = data.map { it.v }
-
-    var boundX = p.map { it.first }.minMax()!!
-    var boundY = p.map { it.second }.minMax()!!
+    var positions = initialPositions
     var c = 0
-    while (boundY.second - boundY.first != 9) {
-        p = p.mapIndexed { index, p -> p + v[index] }
+    do {
+        positions = positions.mapIndexed { index, p -> p + velocities[index] }
+        val boundY = positions.map { it.second }.minMax()!!
         c++
-        boundX = p.map { it.first }.minMax()!!
-        boundY = p.map { it.second }.minMax()!!
-    }
+    } while (boundY.size() != 9)
 
-    for (y in boundY.first..boundY.second) {
-        for (x in boundX.first..boundX.second) {
-            val c = x to y
-            print(if (p.contains(c)) "#" else '.')
+    val boundY = positions.map { it.second }.minMax()!!
+    val boundX = positions.map { it.first }.minMax()!!
+    for (y in boundY.toRange()) {
+        for (x in boundX.toRange()) {
+            print(if (positions.contains(x to y)) "#" else '.')
         }
         println()
     }
-    println(c)
 
-    return ""
-}
-
-fun part2(puzzle: List<String>): Any {
-    return ""
+    return c
 }
 
 fun main(args: Array<String>) {
-    val puzzle = readPuzzle(10)
+    val puzzle = readPuzzle(10) { it.extractAllInts().toList() }
 
-    println(part1(puzzle))
-    println(part2(puzzle))
+    println(part1and2(puzzle))
 }
