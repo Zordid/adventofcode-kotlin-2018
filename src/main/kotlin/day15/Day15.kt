2276d5b74f0149729e2bf4fd8a94d903ade5881a
Zordid <Zordid@users.noreply.github.com>
2018-12-15 15:34:38 +0100

    Improve day 15

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,170 +1,173 @@
 package day15
 
-import day11.allCoordinates
 import shared.breadthFirstSearch
 import shared.readPuzzle
 
 data class Coordinate(val x: Int, val y: Int) : Comparable<Coordinate> {
+    val neighbors get() = deltas.map { transposeBy(it) }
+
     override fun compareTo(other: Coordinate) =
         if (y == other.y) x.compareTo(other.x) else y.compareTo(other.y)
 
     infix fun distanceTo(other: Coordinate) = Math.abs(y - other.y) + Math.abs(x - other.x)
-    fun transpose(delta: Pair<Int, Int>) = Coordinate(x + delta.first, y + delta.second)
+
+    private fun transposeBy(delta: Pair<Int, Int>) = Coordinate(x + delta.first, y + delta.second)
+
+    companion object {
+        val deltas = listOf(0 to -1, -1 to 0, 1 to 0, 0 to 1)
+    }
 }
 
-fun List<CharArray>.freeNeighbors(c: Coordinate) = deltas.map { Coordinate(c.x + it.first, c.y + it.second) }
-    .filter { (x, y) -> this[y][x] == '.' }
-
-val deltas = listOf(0 to -1, -1 to 0, 1 to 0, 0 to 1)
-
-fun List<CharArray>.at(c: Coordinate) = this[c.y][c.x]
-
-fun List<CharArray>.shortestPath(s: Coordinate, d: Coordinate) =
-    breadthFirstSearch(s, { p -> this.freeNeighbors(p) }) { it == d }
-
-class Player(val type: Char, x: Int, y: Int, var hitPoints: Int = 200, var attackPoints: Int = 3) : Comparable<Player> {
-    var coordinate = Coordinate(x, y)
-
+class Player(val type: Char, x: Int, y: Int, val attackPoints: Int = 3) : Comparable<Player> {
+    var position = Coordinate(x, y)
+    var hitPoints = 200
     val isAlive get() = hitPoints > 0
 
-    override fun compareTo(other: Player) = coordinate.compareTo(other.coordinate)
-
-    infix fun distanceTo(other: Player) = coordinate distanceTo other.coordinate
-
-    fun freeNeighborFields(map: List<CharArray>) =
-        deltas.map { coordinate.transpose(it) }.filter { map.at(it) == '.' }
-
-    fun enemiesInRange(enemies: List<Player>) = enemies.filter { distanceTo(it) == 1 }
-
-    fun determineTarget(map: List<CharArray>, enemies: List<Player>): Coordinate? {
-        val paths = enemies
+    fun determineTarget(map: List<CharArray>, aliveEnemies: List<Player>): Coordinate? {
+        val reachableTargetsWithDistance = aliveEnemies
             .flatMap { it.freeNeighborFields(map) }
-            .map { it -> it to map.shortestPath(coordinate, it) }
-            .filter { it.second.isNotEmpty() }
-            .sortedBy { it.second.size }
-        if (paths.isEmpty())
+            .map { it to map.shortestPathLength(position, it) }
+            .filter { it.second > 0 }
+            .sortedBy { it.second }
+
+        if (reachableTargetsWithDistance.isEmpty())
             return null
 
-        val shortestDistance = paths.first().second.size
-        return paths.filter { it.second.size == shortestDistance }.sortedBy { it.first }.firstOrNull()?.first
+        val shortestDistance = reachableTargetsWithDistance.first().second
+        return reachableTargetsWithDistance
+            .filter { it.second == shortestDistance }
+            .sortedBy { it.first }
+            .first().first
     }
 
-    fun move(map: List<CharArray>, enemies: List<Player>) {
-        val destination = determineTarget(map, enemies) ?: return
-        val possibleNeighborsWithPath =
-            freeNeighborFields(map).map { it to map.shortestPath(it, destination) }.sortedBy { it.second.size }
-        val min = possibleNeighborsWithPath.first().second.size
+    private fun move(map: List<CharArray>, aliveEnemies: List<Player>) {
+        val destination = determineTarget(map, aliveEnemies) ?: return
 
-        val step = possibleNeighborsWithPath.filter { it.second.size == min }.sortedBy { it.first }.first().first
-        coordinate = step
+        val possibleNeighborsWithPathLength = freeNeighborFields(map)
+            .map { it to map.shortestPathLength(it, destination) }
+            .sortedBy { it.second }
+
+        val min = possibleNeighborsWithPathLength.first().second
+        val step = possibleNeighborsWithPathLength
+            .filter { it.second == min }
+            .sortedBy { it.first }
+            .first().first
+
+        position = step
     }
 
-    fun attack(enemies: List<Player>) {
-        val lowestHitPoints = enemies.filter { it.isAlive }.minBy { it.hitPoints }!!.hitPoints
-        val target = enemies.filter { it.hitPoints == lowestHitPoints }.sorted().first()
+    private fun attack(aliveEnemiesInRange: List<Player>) {
+        val lowestHitPoints = aliveEnemiesInRange.minBy { it.hitPoints }!!.hitPoints
+        val target = aliveEnemiesInRange.filter { it.hitPoints == lowestHitPoints }.sorted().first()
 
         target.hitPoints -= attackPoints
     }
 
-    fun takeTurn(layout: List<CharArray>, players: List<Player>): Boolean {
-        val enemies = players.filter { it.isAlive && it.type != type }
-        if (enemies.isEmpty()) return false
+    fun takeTurn(layout: List<CharArray>, alivePlayers: List<Player>): Boolean {
+        val aliveEnemies = alivePlayers.filter { it.type != type }
+        if (aliveEnemies.isEmpty()) return false
 
-        val map = generateMap(layout, players.filter { it.isAlive } - this)
-        var enemiesInRange = enemiesInRange(enemies)
-        if (enemiesInRange.isEmpty()) {
-            move(map, enemies)
-            enemiesInRange = enemiesInRange(enemies)
+        var aliveEnemiesInRange = enemiesInRange(aliveEnemies)
+        if (aliveEnemiesInRange.isEmpty()) {
+            val map = generateMap(layout, alivePlayers - this)
+            move(map, aliveEnemies)
+            aliveEnemiesInRange = enemiesInRange(aliveEnemies)
         }
-        if (enemiesInRange.isNotEmpty())
-            attack(enemiesInRange)
+        if (aliveEnemiesInRange.isNotEmpty())
+            attack(aliveEnemiesInRange)
         return true
     }
 
-    override fun toString(): String {
-        return "$type($hitPoints)"
+    override fun toString() = "$type($hitPoints)"
+
+    override fun compareTo(other: Player) = position.compareTo(other.position)
+
+    private infix fun distanceTo(other: Player) = position distanceTo other.position
+
+    private fun freeNeighborFields(map: List<CharArray>) = map.freeNeighbors(position)
+
+    private fun enemiesInRange(enemies: List<Player>) = enemies.filter { this distanceTo it == 1 }
+
+}
+
+fun List<CharArray>.at(c: Coordinate) = this[c.y][c.x]
+
+fun List<CharArray>.freeNeighbors(c: Coordinate) =
+    c.neighbors.filter { at(it) == '.' }
+
+fun List<CharArray>.shortestPathLength(s: Coordinate, d: Coordinate) =
+    breadthFirstSearch(s, { p -> this.freeNeighbors(p) }) { it == d }.size - 1
+
+fun List<String>.findAll(d: Char) =
+    mapIndexed { y, s ->
+        s.mapIndexed { x, c ->
+            if (c == d) Coordinate(x, y) else null
+        }.filterNotNull()
+    }.flatten().sorted()
+
+fun generateMap(layout: List<CharArray>, players: List<Player>) =
+    layout.map { it.clone() }.also {
+        players.forEach { p -> it[p.position.y][p.position.x] = p.type }
     }
 
-}
-
-
-fun List<String>.findAll(c: Char) =
-    allCoordinates(this[0].length, size, 0, 0).mapNotNull { (x, y) ->
-        if (this[y][x] == c) Coordinate(x, y) else null
-    }.sorted()
-
-fun List<CharArray>.printMap() {
-    forEach {
-        println(it.joinToString(""))
-    }
-}
-
-fun generateMap(layout: List<CharArray>, players: List<Player>): List<CharArray> {
-    val result = layout.map { it.clone() }
-    players.forEach { p -> result[p.coordinate.y][p.coordinate.x] = p.type }
-    return result
-}
-
-class Combat(puzzle: List<String>, elfPower: Int = 3) {
-
+class Combat(puzzle: List<String>, elfPower: Int = 3, private val logging: Boolean = false) {
     private val elves = puzzle.findAll('E').map { (x, y) -> Player('E', x, y, attackPoints = elfPower) }
     private val goblins = puzzle.findAll('G').map { (x, y) -> Player('G', x, y) }
     val players = (elves + goblins).toList()
-    val layout = puzzle.map { s -> s.replace('E', '.').replace('G', '.').toCharArray() }
+
+    private var fullRoundsPlayed = 0
+
+    val layout = puzzle.map {
+        it.replace('E', '.').replace('G', '.').toCharArray()
+    }
 
     val alivePlayers get() = players.filter { it.isAlive }
 
     fun playRound(): Boolean {
-        players.sorted().forEach {
-            if (it.isAlive)
-                if (!it.takeTurn(layout, players))
+        alivePlayers.sorted().forEach { player ->
+            if (player.isAlive)
+                if (!player.takeTurn(layout, alivePlayers))
                     return false
         }
+        fullRoundsPlayed++
         return true
     }
 
-    fun part1(stopIfElfDied: Boolean = false): Int {
-        var fullRounds = 0
-        val initialElves = elveCount.size
-        while (true) {
-            if (!playRound()) {
+    fun battle(stopOnElfDeath: Boolean = false): Int {
+        log()
+        while (playRound()) {
+            if (stopOnElfDeath && elves.any { !it.isAlive }) return -1
+            log()
+        }
+        val elvesWin = elves.any { it.isAlive }
+        if (stopOnElfDeath && !elvesWin) return -1
 
-                if (alivePlayers.count { it.type == 'E' } == 0) {
-                    break
-                }
-                if (alivePlayers.count { it.type == 'G' } == 0) {
-                    break
-                }
-            }
-            if ((elveCount.size < initialElves) && stopIfElfDied)
-                return -1
-            fullRounds++
+        val remainingHitPoints = alivePlayers.sumBy { it.hitPoints }
+        if (logging) {
+            println("Finished after $fullRoundsPlayed full rounds.")
+            val winner = if (elvesWin) "Elves" else "Goblins"
+            println("$winner win with $remainingHitPoints remaining hit points.")
         }
 
-        val remainingHitpoints = alivePlayers.sumBy { it.hitPoints }
-        return fullRounds * remainingHitpoints
+        return fullRoundsPlayed * remainingHitPoints
     }
 
-    val elveCount get() = players.filter { it.isAlive && it.type == 'E' }
-
-}
-
-fun part1(puzzle: List<String>): Any {
-    return Combat(puzzle).part1()
-}
-
-fun part2(puzzle: List<String>): Any {
-    (4..Int.MAX_VALUE).first {
-        val combat = Combat(puzzle, it)
-        val result = combat.part1(true)
-        if (result>0) {
-            println(it)
-            println(result)
-        }
-        result > 0
+    private fun log() {
+        if (!logging) return
+        println("After $fullRoundsPlayed rounds:")
+        generateMap(layout, alivePlayers).forEach { println(it.joinToString("")) }
+        println(alivePlayers.joinToString())
+        println()
     }
-    return ""
+
 }
+
+fun part1(puzzle: List<String>) = Combat(puzzle).battle()
+
+fun part2(puzzle: List<String>) =
+    (4..Int.MAX_VALUE).asSequence().map {
+        Combat(puzzle, it).battle(true)
+    }.first { it > 0 }
 
 fun main(args: Array<String>) {
     val puzzle = readPuzzle(15)
--- src/test/kotlin/day15/Day15KtTest.kt
+++ src/test/kotlin/day15/Day15KtTest.kt
@@ -2,22 +2,24 @@ package day15
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import shared.readPuzzle
 
 internal class Day15KtTest {
 
-//    #######       #######       #######       #######       #######
-//    #E..G.#       #E.?G?#       #E.@G.#       #E.!G.#       #E.+G.#
-//    #...#.#  -->  #.?.#?#  -->  #.@.#.#  -->  #.!.#.#  -->  #...#.#
-//    #.G.#G#       #?G?#G#       #@G@#G#       #!G.#G#       #.G.#G#
-//    #######       #######       #######       #######       #######
-
+    //    #######       #######       #######       #######       #######
+    //    #E..G.#       #E.?G?#       #E.@G.#       #E.!G.#       #E.+G.#
+    //    #...#.#  -->  #.?.#?#  -->  #.@.#.#  -->  #.!.#.#  -->  #...#.#
+    //    #.G.#G#       #?G?#G#       #@G@#G#       #!G.#G#       #.G.#G#
+    //    #######       #######       #######       #######       #######
     @Test
     fun testBasics() {
-        var p = """#######
-#E..G.#
-#...#.#
-#.G.#G#
-#######""".split("\n")
+        val p = """
+                #######
+                #E..G.#
+                #...#.#
+                #.G.#G#
+                #######
+                """.trimIndent().split("\n")
 
         val combat = Combat(p)
         val generatedMap = generateMap(combat.layout, combat.players)
@@ -29,154 +31,133 @@ internal class Day15KtTest {
         )
     }
 
-//    #######       #######       #######       #######       #######
-//    #.E...#       #.E...#       #.E...#       #4E212#       #..E..#
-//    #...?.#  -->  #...!.#  -->  #...+.#  -->  #32101#  -->  #.....#
-//    #..?G?#       #..!G.#       #...G.#       #432G2#       #...G.#
-//    #######       #######       #######       #######       #######
-
+    //    #######       #######       #######       #######       #######
+    //    #.E...#       #.E...#       #.E...#       #4E212#       #..E..#
+    //    #...?.#  -->  #...!.#  -->  #...+.#  -->  #32101#  -->  #.....#
+    //    #..?G?#       #..!G.#       #...G.#       #432G2#       #...G.#
+    //    #######       #######       #######       #######       #######
     @Test
-    fun testFirstStep() {
-        val p = """#######
-#.E...#
-#.....#
-#...G.#
-#######""".split("\n")
+    fun firstStepTowardsEnemy() {
+        val p = """
+                #######
+                #.E...#
+                #.....#
+                #...G.#
+                #######
+                """.trimIndent().split("\n")
 
         val combat = Combat(p)
-        val generatedMap = generateMap(combat.layout, combat.players)
         val firstElf = combat.players.first()
 
-        assertEquals(Coordinate(2, 1), firstElf.coordinate)
-        firstElf.move(generatedMap, combat.players.filter { it.type == 'G' })
-
-        assertEquals(Coordinate(3, 1), firstElf.coordinate)
-
-
+        assertEquals(Coordinate(2, 1), firstElf.position)
+        combat.playRound()
+        assertEquals(Coordinate(3, 1), firstElf.position)
     }
 
-//    #########       #########
-//    #G......#       #.G.....#   G(137)
-//    #.E.#...#       #G.G#...#   G(200), G(200)
-//    #..##..G#       #.G##...#   G(200)
-//    #...##..#  -->  #...##..#
-//    #...#...#       #.G.#...#   G(200)
-//    #.G...G.#       #.......#
-//    #.....G.#       #.......#
-//    #########       #########
-
-
+    //    #########       #########
+    //    #G......#       #.G.....#   G(137)
+    //    #.E.#...#       #G.G#...#   G(200), G(200)
+    //    #..##..G#       #.G##...#   G(200)
+    //    #...##..#  -->  #...##..#
+    //    #...#...#       #.G.#...#   G(200)
+    //    #.G...G.#       #.......#
+    //    #.....G.#       #.......#
+    //    #########       #########
     @Test
-    fun minidemo1() {
-        val p = """#######
-#.G...#
-#...EG#
-#.#.#G#
-#..G#E#
-#.....#
-#######""".split("\n")
+    fun miniDemo1() {
+        val p = """
+                #######
+                #.G...#
+                #...EG#
+                #.#.#G#
+                #..G#E#
+                #.....#
+                #######
+                """.trimIndent().split("\n")
 
-        val c = Combat(p)
-
-        assertEquals(27730, c.part1())
-
+        assertEquals(27730, Combat(p).battle())
     }
 
     @Test
     fun demo1a() {
-        val p = """#######
-#G..#E#
-#E#E.E#
-#G.##.#
-#...#E#
-#...E.#
-#######""".split("\n")
-        val c = Combat(p)
-        assertEquals(36334, c.part1())
-
+        val p = """
+                #######
+                #G..#E#
+                #E#E.E#
+                #G.##.#
+                #...#E#
+                #...E.#
+                #######
+                """.trimIndent().split("\n")
+        assertEquals(36334, Combat(p).battle())
     }
 
-//    #######       #######
-//    #E..EG#       #.E.E.#   E(164), E(197)
-//    #.#G.E#       #.#E..#   E(200)
-//    #E.##E#  -->  #E.##.#   E(98)
-//    #G..#.#       #.E.#.#   E(200)
-//    #..E#.#       #...#.#
-//    #######       #######
-
+    //    #######       #######
+    //    #E..EG#       #.E.E.#   E(164), E(197)
+    //    #.#G.E#       #.#E..#   E(200)
+    //    #E.##E#  -->  #E.##.#   E(98)
+    //    #G..#.#       #.E.#.#   E(200)
+    //    #..E#.#       #...#.#
+    //    #######       #######
     @Test
     fun demo1b() {
-        val p = """#######
-#E..EG#
-#.#G.E#
-#E.##E#
-#G..#.#
-#..E#.#
-#######""".split("\n")
-        val c = Combat(p)
-        assertEquals(39514, c.part1())
+        val p = """
+                #######
+                #E..EG#
+                #.#G.E#
+                #E.##E#
+                #G..#.#
+                #..E#.#
+                #######
+                """.trimIndent().split("\n")
+        assertEquals(39514, Combat(p).battle())
     }
 
-//    #######       #######
-//    #.E...#       #.....#
-//    #.#..G#       #.#G..#   G(200)
-//    #.###.#  -->  #.###.#
-//    #E#G#G#       #.#.#.#
-//    #...#G#       #G.G#G#   G(98), G(38), G(200)
-//    #######       #######
-
+    //    #######       #######
+    //    #.E...#       #.....#
+    //    #.#..G#       #.#G..#   G(200)
+    //    #.###.#  -->  #.###.#
+    //    #E#G#G#       #.#.#.#
+    //    #...#G#       #G.G#G#   G(98), G(38), G(200)
+    //    #######       #######
     @Test
     fun demo1y() {
-        val p = """#######
-#.E...#
-#.#..G#
-#.###.#
-#E#G#G#
-#...#G#
-#######""".split("\n")
-        val c = Combat(p)
-        assertEquals(28944, c.part1())
+        val p = """
+                #######
+                #.E...#
+                #.#..G#
+                #.###.#
+                #E#G#G#
+                #...#G#
+                #######
+                """.trimIndent().split("\n")
+        assertEquals(28944, Combat(p).battle())
     }
 
     @Test
     fun demo1z() {
-        val p = """#########
-#G......#
-#.E.#...#
-#..##..G#
-#...##..#
-#...#...#
-#.G...G.#
-#.....G.#
-#########""".split("\n")
-
-        val c = Combat(p)
-        assertEquals(18740, c.part1())
-
+        val p = """
+                #########
+                #G......#
+                #.E.#...#
+                #..##..G#
+                #...##..#
+                #...#...#
+                #.G...G.#
+                #.....G.#
+                #########
+                """.trimIndent().split("\n")
+        assertEquals(18740, Combat(p).battle())
     }
 
     @Test
-    fun moveTest() {
-        val p = """#########
-#G..G..G#
-#.......#
-#.......#
-#G..E..G#
-#.......#
-#.......#
-#G..G..G#
-#########""".split("\n")
-
-        val c = Combat(p)
-
-        generateMap(c.layout, c.alivePlayers).printMap()
-        repeat(3) {
-            c.playRound()
-            println("After ${it + 1}")
-            generateMap(c.layout, c.alivePlayers).printMap()
-        }
-
+    fun part1() {
+        assertEquals(181952, part1(readPuzzle(15)))
     }
 
+    @Test
+    fun part2() {
+        assertEquals(47296, part2(readPuzzle(15)))
+    }
 
 }
