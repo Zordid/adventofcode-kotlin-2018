72a7f0ec2057f8c89a27b06a2dc55c53c4d6e2f8
Zordid <Zordid@users.noreply.github.com>
2018-12-16 19:09:24 +0100

    Improve day 12

--- src/main/kotlin/day12/Day12SubterraneanSustainability.kt
+++ src/main/kotlin/day12/Day12SubterraneanSustainability.kt
@@ -1,109 +1,87 @@
 package day12
 
+import shared.measureRuntime
 import shared.readPuzzle
 
-val seen = mutableMapOf<String, Pair<Long, Long>>()
-var finish = false
-
-
 class Pots(puzzle: List<String>) {
+    private val initial =
+        puzzle.first().split(" ")[2] to 0L
+    private val transforms =
+        puzzle.filter { it.contains("=>") }
+            .map { l -> l.split(" ").let { it[0] to it[2] } }.toMap()
 
-    val initial = puzzle[0].substring(puzzle[0].indexOf(": ") + 2) to 0L
-    val transforms =
-        puzzle.filter { it.contains("=>") }.map { it.split(" ").let { it[0] to it[2] } }.toMap()
+    fun solvePart1(): Long {
+        var p = initial
+        repeat(20) { p = p.nextGeneration() }
+        return p.sum()
+    }
+
+    fun solvePart2(): Long {
+        var leftGenerations = 50000000000L
+        var prev: Pair<String, Long>
+        var pots = initial
+        do {
+            prev = pots
+            pots = prev.nextGeneration()
+            leftGenerations--
+        } while (pots.first != prev.first)
+        val drift = pots.second - prev.second
+        println("Stabilized with $leftGenerations to go!")
+        println("Drift per generation is $drift.")
+
+        pots = pots.first to pots.second + drift * leftGenerations
+        return pots.sum()
+    }
+
+    fun solveAny(g: Long): Long {
+        if (g == 0L) return initial.sum()
+        var leftGenerations = g
+        val seen = mutableMapOf<String, Pair<Long, Long>>()
+        var pots = initial
+        do {
+            seen[pots.first] = leftGenerations to pots.second
+            pots = pots.nextGeneration()
+            leftGenerations--
+        } while (!seen.contains(pots.first) && leftGenerations > 0)
+        if (leftGenerations == 0L) return pots.sum()
+
+        val before = seen[pots.first]!!
+        val drift = pots.second - before.second
+        val loopSize = before.first - leftGenerations
+        println("Stabilized with $leftGenerations to go!")
+        println("Drift per generation is $drift and loop size $loopSize.")
+        val skipLoops = leftGenerations / loopSize
+        leftGenerations %= loopSize
+        pots = pots.first to pots.second + drift * skipLoops
+        println("Skipping $skipLoops loops and finishing $leftGenerations loops..")
+        repeat(leftGenerations.toInt()) { pots = pots.nextGeneration() }
+        return pots.sum()
+    }
 
     private fun Pair<String, Long>.nextGeneration(): Pair<String, Long> {
-        val newPattern = "....${this.first}...."
+        val (pattern, start) = this
+        val newPattern = "....$pattern...."
             .windowed(5).joinToString("") { transforms.getOrDefault(it, ".") }
-        val newStart = this.second - 2 + newPattern.indexOf('#')
+        val newStart = start - 2 + newPattern.indexOf('#')
         return newPattern.trim('.') to newStart
     }
 
     private fun Pair<String, Long>.sum() = first.mapIndexed { idx, c -> (idx + second) to c }
         .fold(0L) { acc, (idx, c) -> if (c == '#') acc + idx else acc }
 
-    fun solvePart1(): Any {
-        var p = initial
-
-        println(p)
-        repeat(20) {
-            p = p.nextGeneration()
-            println(p)
-        }
-        return p.sum()
-    }
-
 }
 
-private tailrec fun Pair<String, Long>.transform(
-    transforms: Map<String, String>,
-    generations: Long
-): Pair<String, Long> {
-    val s = "....${this.first}...."
-    var base = this.second - 2
+fun part1(puzzle: List<String>) = Pots(puzzle).solvePart1()
 
-    var r = s.windowed(5).map { transforms.getOrDefault(it, ".") }.joinToString("")
-    val start = r.indexOf('#')
-    r = r.substring(start).trim('.')
-    base += start
-
-    println("$r $base")
-    var result = r to base
-    var genLeft = generations
-
-    if (!finish) {
-        if (seen.contains(r)) {
-            val firstSeen = seen[r]!!
-            val drift = base - firstSeen.first
-            val loopLength = firstSeen.second - generations
-            println("Loop detected between ${firstSeen.second} and $generations")
-            println("loop length is $loopLength")
-            println("drift is $drift")
-
-            genLeft = (generations % loopLength)
-            val skip = generations - genLeft
-            val skipTimes = skip / loopLength
-            println("Skipping $skipTimes iterations and now at gen $genLeft")
-
-            result = r to (base + drift * skipTimes)
-
-            finish = true
-        } else {
-            seen[r] = base to generations
-        }
-    }
-
-    return if (genLeft > 0) result.transform(transforms, genLeft - 1) else result
-}
-
-fun part1(puzzle: List<String>): Any {
-    val initial = puzzle[0].substring(puzzle[0].indexOf(": ") + 2)
-    val transforms = puzzle.filter { it.contains("=>") }.map { it.split(" ").let { it[0] to it[2] } }.toMap()
-
-    seen.clear()
-    finish = false
-    val result = (initial to 0L).transform(transforms, 19)
-
-    return result.first.mapIndexed { idx, c -> idx + result.second to c }
-        .fold(0L) { acc, (idx, c) -> if (c == '#') acc + idx else acc }
-}
-
-fun part2(puzzle: List<String>): Any {
-    val initial = puzzle[0].substring(puzzle[0].indexOf(": ") + 2)
-    val transforms = puzzle.filter { it.contains("=>") }.map { it.split(" ").let { it[0] to it[2] } }.toMap()
-
-    seen.clear()
-    finish = false
-    val result = (initial to 0L).transform(transforms, 50000000000L - 1)
-
-    return result.first.mapIndexed { idx, c -> idx + result.second to c }
-        .fold(0L) { acc, (idx, c) -> if (c == '#') acc + idx else acc }
-
-}
+fun part2(puzzle: List<String>) = Pots(puzzle).solvePart2()
 
 fun main(args: Array<String>) {
     val puzzle = readPuzzle(12)
 
-    println(part1(puzzle))
-    println(part2(puzzle))
+    measureRuntime {
+        println(part1(puzzle))
+    }
+    measureRuntime {
+        println(part2(puzzle))
+    }
 }
--- src/test/kotlin/day12/Day12SubterraneanSustainabilityKtTest.kt
+++ src/test/kotlin/day12/Day12SubterraneanSustainabilityKtTest.kt
@@ -2,33 +2,50 @@ package day12
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import shared.readPuzzle
 
-internal class Day12SubterraneanSustainabilityKtTest {
+class Day12SubterraneanSustainabilityKtTest {
 
-    private val input = """initial state: #..#.#..##......###...###
+    private val demo = """
+        initial state: #..#.#..##......###...###
 
-...## => #
-..#.. => #
-.#... => #
-.#.#. => #
-.#.## => #
-.##.. => #
-.#### => #
-#.#.# => #
-#.### => #
-##.#. => #
-##.## => #
-###.. => #
-###.# => #
-####. => #""".split("\n")
+        ...## => #
+        ..#.. => #
+        .#... => #
+        .#.#. => #
+        .#.## => #
+        .##.. => #
+        .#### => #
+        #.#.# => #
+        #.### => #
+        ##.#. => #
+        ##.## => #
+        ###.. => #
+        ###.# => #
+        ####. => #""".trimIndent().split("\n")
 
     @Test
-    fun part1() {
-        assertEquals(325L, part1(input))
+    fun demo1() {
+        assertEquals(325, part1(demo))
     }
 
     @Test
-    fun demo() {
-        assertEquals(325L, Pots(input).solvePart1())
+    fun part1() {
+        assertEquals(3798, part1(readPuzzle(12)))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(3900000002212, part2(readPuzzle(12)))
+    }
+
+    @Test
+    fun solveAny1() {
+        assertEquals(3798, Pots(readPuzzle(12)).solveAny(20L))
+    }
+
+    @Test
+    fun solveAny2() {
+        assertEquals(3900000002212, Pots(readPuzzle(12)).solveAny(50000000000L))
     }
 }
