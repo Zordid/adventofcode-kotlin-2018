9b01150621b38eaa7b96d4e25e166a98bd318eda
Zordid <Zordid@users.noreply.github.com>
2018-12-19 08:02:04 +0100

    Just a little cleanup day 19

--- src/main/kotlin/day19/Day19.kt
+++ src/main/kotlin/day19/Day19.kt
@@ -25,22 +25,21 @@ val instructionSet = mapOf<String, Instruction>(
     "eqrr" to { a, b, c, r -> r[c] = if (r[a] == r[b]) 1 else 0 }
 )
 
-fun part1(puzzle: List<String>): Any {
+fun part1(puzzle: List<String>, conditionalDebug: (IntArray) -> Boolean = { false }): Any {
     val program =
         puzzle.filter { !it.startsWith('#') }.map { it.substring(0, 4) to it.extractAllPositiveInts().toList() }
-    var ipBinding = puzzle.first().extractAllInts().first()
+    val ipBinding = puzzle.first().extractAllInts().first()
     val registers = IntArray(6)
     var ip = 0
 
     registers[0] = 0
     while (ip in program.indices) {
-        var debug = false
 
         registers[ipBinding] = ip
         val (mnemonic, operands) = program[ip]
         val (a, b, c) = operands
 
-        debug = ip == 1
+        val debug = conditionalDebug(registers)
         if (debug)
             print("ip=$ip [${registers.joinToString()}] $mnemonic ${operands.joinToString(" ")} ")
         instructionSet[mnemonic]!!(a, b, c, registers)
@@ -52,108 +51,70 @@ fun part1(puzzle: List<String>): Any {
     }
 
     return registers[0]
-
 }
 
-fun part2(puzzle: List<String>): Any {
-//    #ip 4
-    var r0 = 1
+fun part2(startR0: Int = 1): Any {
+    var r0 = startR0
     var r1 = 0
     var r2 = 0
     var r3 = 0
-    var instructionPointer = 0
-    var r5 = 0
-
+    val instructionPointer = 0
+    val r5 = 0
 
     // CO ROUTINE JUMPED TO BY 0
     fun setup() {
 // 17  addi 1 2 1
-        r1 = r1 + 2
-        instructionPointer = 18
 // 18  mulr 1 1 1
-        r1 = r1 * r1
-        instructionPointer++
 // 19  mulr 4 1 1
-        r1 = instructionPointer * r1
-        instructionPointer++
 // 20  muli 1 11 1
-        r1 = r1 * 11
-        instructionPointer++
+        r1 = 2 * 2 * 11 * 19
 // 21  addi 3 3 3
-        r3 = r3 + 3
-        instructionPointer++
 // 22  mulr 3 4 3
-        r3 = r3 * instructionPointer
-        instructionPointer++
 // 23  addi 3 9 3
-        r3 = r3 + 9
-        instructionPointer++
+        r3 += 3 * 22 + 9
 // 24  addr 1 3 1
-        r1 = r1 + r3
-        instructionPointer++
+        r1 += r3
 // 25  addr 4 0 4
-        instructionPointer += r0
-        instructionPointer++
-        if (r0 == 0) {
-// 26  seti 0 1 4
-            instructionPointer = 0
-            instructionPointer++
-            // JUMP TO 1
-        } else {
+        if (r0 != 0) {
 // 27  setr 4 9 3
-            r3 = instructionPointer
-            instructionPointer++
 // 28  mulr 3 4 3
-            r3 = r3 * instructionPointer
-            instructionPointer++
 // 29  addr 4 3 3
-            r3 = instructionPointer + r3
-            instructionPointer++
+            r3 = 27 * 28 + 29
 // 30  mulr 4 3 3
-            r3 = instructionPointer * r3
-            instructionPointer++
 // 31  muli 3 14 3
-            r3 = r3 * 14
-            instructionPointer++
 // 32  mulr 3 4 3
-            r3 = r3 * instructionPointer
-            instructionPointer++
+            r3 *= 30 * 14 * 32
 // 33  addr 1 3 1
-            r1 = r1 + r3
-            instructionPointer++
+            r1 += r3
 // 34  seti 0 6 0
             r0 = 0
 // 35  seti 0 7 4
-            instructionPointer = 0
-            instructionPointer++
-            // JUMP TO 1
+            // JUMP TO 1 - EXIT SETUP ROUTINE
         }
     }
-
 
 //  0  addi 4 16 4
     // JUMP TO 17
     setup()
     println("$r0 $r1 $r2 $r3 $instructionPointer $r5")
 
-    // r1
-
     r2 = 1
     do {
-        r5 = 1
-        // r5 = 1..r1
-        // addiert r2 zu r0 wenn ein r5
+        // r5 runs from 1 to r1
+        // adds r2 to r0 only if r1 is divisible by r2!
+        // => result is the sum of all prime factors including 1 and the number r1 itself!
         if (r1 % r2 == 0)
             r0 += r2
+//        r5 = 1
 //        do {
 //            if (r2 * r5 == r1) {
 //                r0 += r2
-//                continue
 //            }
 //            r5++
 //        } while (r5 <= r1)
         r2++
     } while (r2 <= r1)
+
     return r0
 }
 
@@ -161,5 +122,5 @@ fun main(args: Array<String>) {
     val puzzle = readPuzzle(19)
 
     println(part1(puzzle))
-    println(part2(puzzle))
+    println(part2(1))
 }
